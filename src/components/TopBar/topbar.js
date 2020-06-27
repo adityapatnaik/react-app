2d9cbc9974b53d679877043bbde4ad6925c30ea8
Aditya Patnaik <adityapatnaik25x@gmail.com>
2020-06-27 13:01:36 +0530

    origin

--- src/components/TopBar/topbar.js
+++ src/components/TopBar/topbar.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function TopBar(props) {
     return (
         <div class="container">
-        <ul>
+        <ul id="menu" class="col-12 mx-auto p-3">
         <li>Title</li>
         <li>Articles</li>
         <li>Greetings</li>
