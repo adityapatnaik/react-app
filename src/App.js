3fdfe186287d12b7a538705c0403ff4efeceed7e
Aditya Patnaik <adityapatnaik25x@gmail.com>
2020-06-27 12:42:22 +0530

    origin

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React from 'react';
+import TopBar from './components/TopBar/topbar'
+
+function App() {
+  return (
+   <>
+   <TopBar/>
+   </>
+  );
+}
+
+
+export default App;
---
+++ src/components/TopBar/topbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './topbar.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function TopBar(props) {
+    return (
+        <div class="container">
+        <ul>
+        <li>Title</li>
+        <li>Articles</li>
+        <li>Greetings</li>
+        </ul>
+        </div>
+      
+    )
+}
